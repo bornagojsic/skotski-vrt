f77cf842d9dc1f75d983dfe33f32701da06982d6
bornagojsic <69301624+bornagojsic@users.noreply.github.com>
2023-02-03 18:20:08 +0100

    Board is initialized only once
    And changed indentation to tabs

--- simulation.py
+++ simulation.py
@@ -6,64 +6,65 @@ from player import Detective, MrX
 from game import Game
 
 class Simulation():
-    def __init__(self):
-        self.mrx_wins = 0
-        self.det_wins = 0
+	def __init__(self):
+		self.mrx_wins = 0
+		self.det_wins = 0
 
-    def play_random(self, game): #Plays a random move
-        game.turn += 1 #If this was after line 23, the game would get into an infinite loop, initial turn is -1 (line 53)
-        game.turn %= len(game.players)
-        if (game.turn == 0):
-            game.round += 1
+	def play_random(self, game): #Plays a random move
+		game.turn += 1 #If this was after line 23, the game would get into an infinite loop, initial turn is -1 (line 53)
+		game.turn %= len(game.players)
+		if (game.turn == 0):
+			game.round += 1
 
-        player = game.players[game.turn]
+		player = game.players[game.turn]
 
-        moves = game.get_legal_moves(player)
-        rnd1 = np.random.randint(0, 4)
-        if sum( [len(moves[x]) for x in range (4)] ) == 0: #If there are no legal moves at all, return
-            return
-        while len(moves[rnd1]) == 0: #Can't pick a move when there are no moves to pick with that vehicle
-            rnd1 = np.random.randint(0, 4)
-        rnd2 = np.random.randint(0, len(moves[rnd1]))
-        position = moves[rnd1][rnd2]
-        vehicle = rnd1
+		moves = game.get_legal_moves(player)
+		rnd1 = np.random.randint(0, 4)
+		if sum( [len(moves[x]) for x in range (4)] ) == 0: #If there are no legal moves at all, return
+			return
+		while len(moves[rnd1]) == 0: #Can't pick a move when there are no moves to pick with that vehicle
+			rnd1 = np.random.randint(0, 4)
+		rnd2 = np.random.randint(0, len(moves[rnd1]))
+		position = moves[rnd1][rnd2]
+		vehicle = rnd1
 
-        game.make_move(player, vehicle, position)
+		game.make_move(player, vehicle, position)
 
-    def simulate_game(self, game): #Simulates one game from the starting gamestate (input)
-        while not game.is_over():
-            self.play_random(game)
-        
-        if (game.round >= MAX_ROUNDS):
-            self.mrx_wins += 1
-        else:
-            self.det_wins += 1
-        return
+	def simulate_game(self, game): #Simulates one game from the starting gamestate (input)
+		while not game.is_over():
+			self.play_random(game)
+		
+		if (game.round >= MAX_ROUNDS):
+			self.mrx_wins += 1
+		else:
+			self.det_wins += 1
+		return
 
-    def play_n_randoms(self, n): #Simulates n random games
-        for i in range (n):
-            board = Board("tax.txt", "bus.txt", "udg.txt", "rvr.txt")
+	def play_n_randoms(self, n): #Simulates n random games
+		board = Board("tax.txt", "bus.txt", "udg.txt", "rvr.txt")
+		for i in range (n):
 
-            d1 = Detective("Detective 1")
-            d2 = Detective("Detective 2")
-            mrx = MrX("Mr. X")
-            detectives = [d1, d2]
+			d1 = Detective("Detective 1")
+			d2 = Detective("Detective 2")
+			mrx = MrX("Mr. X")
+			detectives = [d1, d2]
 
-            game = Game(detectives, mrx, board)
-            game.turn = -1 #Because it increments at the beginning (line 14)
-            self.simulate_game(game)
+			game = Game(detectives, mrx, board)
+			game.turn = -1 #Because it increments at the beginning (line 14)
+			self.simulate_game(game)
 
-    def make_chart(self): #Pretty charts
-        plt.pie([self.mrx_wins, self.det_wins], labels=["Mr. X wins", "Detectives wins"])
-        plt.show()
-        print("Mr. X wins / detectives wins:", self.mrx_wins / (self.det_wins + self.mrx_wins))
+	def make_chart(self): #Pretty charts
+		plt.pie([self.mrx_wins, self.det_wins], labels=["Mr. X wins", "Detectives wins"])
+		plt.show()
+		print("Mr. X wins / detectives wins:", self.mrx_wins / (self.det_wins + self.mrx_wins))
 
 
 def main():
-    n = int(input())
-    sim = Simulation()
-    sim.play_n_randoms(n)
-    sim.make_chart()
+	# n = int(input())
+	n = 100
+	sim = Simulation()
+	sim.play_n_randoms(n)
+	# sim.make_chart()
 
 if __name__ == '__main__':
-    main()
+	main()
