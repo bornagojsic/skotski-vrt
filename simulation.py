9ac36d0680861f7d730002b7c9ab3c0e18d63ea7
bornagojsic <69301624+bornagojsic@users.noreply.github.com>
2023-02-04 01:36:48 +0100

    Changed randint to random.random
    Also, moved the creation of objects outside the for loop

--- simulation.py
+++ simulation.py
@@ -1,9 +1,11 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from random import random
 from constants import *
 from board import Board
 from player import Detective, MrX
 from game import Game
+
 
 class Simulation():
 	def __init__(self):
@@ -19,14 +21,19 @@ class Simulation():
 		player = game.players[game.turn]
 
 		moves = game.get_legal_moves(player)
-		rnd1 = np.random.randint(0, 4)
-		if sum( [len(moves[x]) for x in range (4)] ) == 0: #If there are no legal moves at all, return
+		rnd1 = int(random() * 4)
+		if not any( [(moves[x]) for x in range (4)] ): #If there are no legal moves at all, return
+			# print(f"{player.name} has no legal moves!")
+			# print(moves)
+			# [print(player.positions) for player in game.players]
 			return
 		while len(moves[rnd1]) == 0: #Can't pick a move when there are no moves to pick with that vehicle
-			rnd1 = np.random.randint(0, 4)
-		rnd2 = np.random.randint(0, len(moves[rnd1]))
+			rnd1 = int(random() * 4)
+		rnd2 = int(random() * len(moves[rnd1]))
 		position = moves[rnd1][rnd2]
 		vehicle = rnd1
+
+		# print(f"{player.name} made a move {vehicle} to {position}")
 
 		game.make_move(player, vehicle, position)
 
@@ -35,24 +42,28 @@ class Simulation():
 			self.play_random(game)
 		
 		if (game.round >= MAX_ROUNDS):
+			# print("Mr. X wins!")
 			self.mrx_wins += 1
 		else:
+			# print("Detectives win!")
 			self.det_wins += 1
 		return
 
 	def play_n_randoms(self, n): #Simulates n random games
 		board = Board("tax.txt", "bus.txt", "udg.txt", "rvr.txt")
-		for i in range (n):
-			d1 = Detective("Detective 1")
-			d2 = Detective("Detective 2")
-			mrx = MrX("Mr. X")
+		
+		
+		d1 = Detective("Detective 1")
+		d2 = Detective("Detective 2")
+		mrx = MrX("Mr. X")
+		for _ in range(n):
 			detectives = [d1, d2]
-
 			game = Game(detectives, mrx, board)
-			game.turn = -1 #Because it increments at the beginning (line 14)
+			game.turn = -1 # Because it increments at the beginning (line 14)
 			self.simulate_game(game)
+			[player.reset() for player in game.players]
 
-	def make_chart(self): #Pretty charts
+	def make_chart(self): # Pretty charts
 		plt.pie([self.mrx_wins, self.det_wins], labels=["Mr. X wins", "Detectives wins"])
 		plt.show()
 		print("Mr. X wins / detectives wins:", self.mrx_wins / (self.det_wins + self.mrx_wins))
@@ -60,10 +71,10 @@ class Simulation():
 
 def main():
 	# n = int(input())
-	n = 1000
+	n = int(1e5)
 	sim = Simulation()
 	sim.play_n_randoms(n)
-	# sim.make_chart()
+	sim.make_chart()
 
 if __name__ == '__main__':
 	main()
