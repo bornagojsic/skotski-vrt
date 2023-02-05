5aef4b1b39f8aae82a7b2c9d87ff43cc8195e6a8
bornagojsic <69301624+bornagojsic@users.noreply.github.com>
2023-02-05 11:51:07 +0100

    Added the real Scotland Yard map

--- constants.py
+++ constants.py
@@ -1,4 +1,4 @@
-MAX_STATION = 5
+MAX_STATION = 199
 
 DETECTIVE_TAX = 10
 DETECTIVE_BUS = 8
@@ -9,7 +9,7 @@ MRX_UDG = 2
 MRX_RVR = 5
 MRX_X2 = 2
 
-MAX_ROUNDS = 3
+MAX_ROUNDS = 20
 
 vehicle_to_idx = {"TAX": 0, "BUS": 1, "UDG": 2, "RVR": 3, "X2": 4}
 idx_to_vehicle = {0: "TAX", 1: "BUS", 2: "UDG", 3: "RVR", 4: "X2"}
