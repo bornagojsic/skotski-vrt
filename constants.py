f9c3e6ba858ab9cd070ae92dab268caf01c4fbe8
Ivan Bevanda <86872168+BevandaIvan@users.noreply.github.com>
2023-02-08 07:54:07 +0100

    added C_parameter

--- constants.py
+++ constants.py
@@ -5,5 +5,7 @@ MRX_CARDS = [4, 3, 2, 5, 2]
 
 MAX_ROUNDS = 20
 
+C_PARAMETER = 1.41421356
+
 vehicle_to_idx = {"TAX": 0, "BUS": 1, "UDG": 2, "RVR": 3, "X2": 4}
 idx_to_vehicle = {0: "TAX", 1: "BUS", 2: "UDG", 3: "RVR", 4: "X2"}
