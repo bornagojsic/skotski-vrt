8d11f8b974a1a8bcadec0ccba53f498f7335af72
Ivan Bevanda <86872168+BevandaIvan@users.noreply.github.com>
2023-02-08 14:31:44 +0100

    Should work?

--- mcts.py
+++ mcts.py
@@ -6,29 +6,32 @@ from game import Game
 from simulation import Simulation
 from copy import deepcopy
 
+NUM_ITERS = 10000
+
 class Node():
 
     def __init__(self, game, ind, parent_ind, parent_move_x, parent_move_y):
         self.positions = [player.position for player in game.players]
-        self.cards = [player.cards[:] for player in game.players]
-        self.player = game.players[game.turn]
+        self.cards = [player.cards[:] for player in game.players] #Deepcopy of card lists
+        self.player = game.players[game.turn] 
+        self.turn = game.turn
+        self.round = game.round
         self.is_terminal = game.is_over()
 
         self.wins = 0
-        self.losses = 0
         self.visits = 0
-        self.ind = ind
+        self.ind = ind #Index of the node in the game tree (MCTS.tree is an array of nodes)
         self.parent_ind = parent_ind
 
-        self.parent_move_x = 0
-        self.parent_move_y = 0
+        self.parent_move_x = parent_move_x #See MCTS.expansion comments
+        self.parent_move_y = parent_move_y
         
         self.legal_moves = game.get_legal_moves(player=game.players[game.turn])
         self.num_of_legal_moves = sum(len(l) for l in self.legal_moves)
-        self.children = []
+        self.children = [] #Stores indices (in MCTS.tree) of children 
         self.has_children = False
-        self.next_move_x = 0 #Very non-elegant solution
-        self.next_move_y = 0 ############### as a start
+        self.next_move_x = 0 #See MCTS.expansion comments
+        self.next_move_y = 0 
 
         self.nodes_expanded = 0
         self.all_expanded = False
@@ -36,51 +39,50 @@ class Node():
 class MCTS():
 
     def __init__(self, num_iters, gamestate):
-        self.tree = [0] * (num_iters + 1)
-        self.max_ind = 0
-        self.game = deepcopy(gamestate)
+        self.tree = [0] * (num_iters + 1) #Tree is initialized as an array of ints to avoid appending
+        self.max_ind = 0 #Index of the last created node in the tree
+        self.game = deepcopy(gamestate) #Deepcopy of the gamestate which will then be updated when neccessary
         self.sim = Simulation()
 
-    def UCB1(self, node):
+    def UCB1(self, node): #Calculates UCT score for each child, formula can be found online (MCTS wikipedia - selection)
         ret = []
-        for ind in node.children:
+        for ind in node.children: 
             temp = self.tree[ind]
             if temp.visits == 0:
-                ret.append(123456789) #inf
+                ret.append(123456789) #inf if never visited
                 continue
-            
-            if temp.player != 0:
-                wr = 1 - temp.wins/temp.visits
-            else:
-                wr = temp.wins/temp.visits
-            ret.append(wr + C_PARAMETER * (np.log(node.visits) / temp.visits) ) #UCB1 formula
+            wr = temp.wins/temp.visits #Mr.x. wins
+            if node.turn != 0: #Detectives are minimizing agents!
+                wr = 1 - wr
+            ret.append(wr + C_PARAMETER * (np.log(node.visits) / temp.visits) ) #UCT formula
         return ret
 
-    def selection(self, node):
-        while True:
+    def selection(self, node): #Descends down the tree according to UCT until it reaches
+        while True:            # a leaf node
             if not node.all_expanded:
                 return node
             if node.has_children:
-                selected_ind = np.argmax(self.UCB1(node))
-                node = self.tree[node.children[selected_ind]]
+                selected_ind = np.argmax(self.UCB1(node)) #Index of max value
+                node = self.tree[node.children[selected_ind]] #Selects the best child and descends one level down
                 continue
             break
         return node
 
-    def expansion(self, node):
+    def expansion(self, node): #Creates a new node from the selected node (which we got to by MCTS.selection)
         if node.is_terminal:
             return
         if not node.all_expanded:
-            self.game.set_state(node.positions, node.cards)
-            
-            vehicle = node.next_move_x #coordinates in a jagged array
-            position = node.legal_moves[node.next_move_x][node.next_move_y]
+            self.game.set_state(node.positions, node.cards, node.turn, node.round) #Sets game state to the one remembered 
+                                                                                   #by the node
+
+            vehicle = node.next_move_x # legal_moves is a jagged array, so I use two coordinates to expand one move
+            position = node.legal_moves[node.next_move_x][node.next_move_y] # and remember which move I expanded
             
             self.game.make_move(node.player, vehicle, position)
             self.game.turn = (self.game.turn + 1) % len(self.game.players)
 
             self.max_ind += 1
-            child = Node(self.game, self.max_ind, node.ind, node.next_move_x, node.next_move_y)
+            child = Node(self.game, self.max_ind, node.ind, node.next_move_x, node.next_move_y) #creates the child node
             self.tree[self.max_ind] = child
             node.children.append(child.ind)
             node.nodes_expanded += 1
@@ -88,7 +90,7 @@ class MCTS():
             if node.nodes_expanded == node.num_of_legal_moves:
                 node.all_expanded = True
 
-            node.next_move_y += 1 #Sets up indices for the next move
+            node.next_move_y += 1 #Sets up indices so that the next expansion call on this node expands the next move
             while node.next_move_y >= len(node.legal_moves[node.next_move_x]):
                 if (node.next_move_x >= 4):
                     break
@@ -99,16 +101,16 @@ class MCTS():
     def simulation(self, node):
         self.sim.det_wins = 0
         self.sim.mrx_wins = 0
-        self.game.set_state(node.positions, node.cards)
+        self.game.set_state(node.positions, node.cards, node.turn, node.round) #Sets game state to the one remember in the node
         
         self.sim.simulate_game(self.game)
 
-        if self.sim.det_wins > 0:
+        if self.sim.det_wins > 0: #Detectives won -> 0
             return 0
-        if self.sim.mrx_wins > 0:
+        if self.sim.mrx_wins > 0: #Model won -> 1
             return 1
 
-    def backpropagation(self, node, result):
+    def backpropagation(self, node, result): #Updates the weights of the simulated path
         node.visits += 1
         node.wins += result
         
@@ -116,11 +118,11 @@ class MCTS():
             return
         self.backpropagation(self.tree[node.parent_ind], result)
 
-    def create_root_node(self):
+    def create_root_node(self): #Self-explanatory
         self.tree[0] = Node(self.game, 0, None, None, None)
         self.tree[0].visits = 1
 
-    def search(self, num_iters):
+    def search(self, num_iters): # Oh yeah, it's all coming together
         self.create_root_node()
         node = self.tree[0]
         for _ in range (num_iters):
@@ -130,11 +132,16 @@ class MCTS():
             result = self.simulation(node)
             self.backpropagation(node, result)
         node = self.tree[0]
+        
         weights = [self.tree[x].visits for x in node.children]
         print("Weights:", weights)
-        node = self.tree[node.children[np.argmax(weights)]]
+        
+        weights2 = [self.tree[x].wins for x in node.children]
+        print("Wins:", weights2)
+        
+        node = self.tree[node.children[np.argmax(weights)]] #Selects the most-visited child
         vehicle = node.parent_move_x
-        position = self.tree[0].legal_moves[node.parent_move_x][node.parent_move_y]
+        position = self.tree[0].legal_moves[node.parent_move_x][node.parent_move_y] #Gets the move which leads to said child
         return [vehicle, position]
         
 
@@ -146,45 +153,10 @@ def main():
     detectives = [d1, d2]
     game = Game(detectives, mrx, board)
     game.turn = 0
-    mcts = MCTS(100000, game)
+    mcts = MCTS(NUM_ITERS, game)
 
-    print(mcts.search(100000))
+    print(mcts.search(NUM_ITERS))
     print(mcts.tree[0].legal_moves)
-
-    """#Creates root node
-    mcts.create_root_node()
-    print("Root legal moves:", mcts.tree[0].legal_moves)
-    print("------------------------------------------")
-    
-    #Tests if expansion works
-    for ind in range(sum(len(l) for l in mcts.tree[0].legal_moves)):
-        print(mcts.tree[0].next_move_x, mcts.tree[0].next_move_y)
-        mcts.expansion(mcts.tree[0])
-        print("Nodes expanded:", mcts.tree[0].all_expanded)
-        print("Max ind:", mcts.max_ind)
-        print("Len of children:", len(mcts.tree[0].children))
-        print(f"Child {ind} legal moves:", mcts.tree[mcts.max_ind].legal_moves)
-        print("------------------------------------------")
-        for _ in range(sum(len(l) for l in mcts.tree[ind].legal_moves)):
-            mcts.expansion(mcts.tree[ind])
-    print("Children of root node:", mcts.tree[0].children)
-    print("Number of nodes:", mcts.max_ind)
-    print("Parent of last node:", mcts.tree[mcts.max_ind].parent_ind)
-    print("Legal moves of last node:", mcts.tree[mcts.max_ind].legal_moves)
-    
-    #Tests if selection works
-    ind_leaf = mcts.tree[1].children[0]
-    print("First leaf node:", ind_leaf)
-    mcts.tree[ind_leaf].wins = 10
-    mcts.tree[ind_leaf].visits = 20
-    leaf = mcts.selection(mcts.tree[0]).ind
-    print("Selected node:", leaf)
-    print("UCB1 scores of first child of root", mcts.UCB1(mcts.tree[1]))
-    print("Root wins, visits:", mcts.tree[0].wins, mcts.tree[0].visits)
-    result = mcts.simulation(mcts.tree[leaf])
-    print("Result of simulation:", result)
-    mcts.backpropagation(mcts.tree[leaf], result)
-    print("Root wins, visits:", mcts.tree[0].wins, mcts.tree[0].visits)"""
 
 if __name__ == '__main__':
     main()
