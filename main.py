e412eec36d9c9f3c3f8be5562c735a8e92f5bd45
Ivan Bevanda <86872168+BevandaIvan@users.noreply.github.com>
2023-02-08 22:07:23 +0100

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ class Playing():
 		d3 = Detective("Detective 3")
 		d4 = Detective("Detective 4")
 		mrx = MrX("Mr. X")
-		detectives = [d1, d2, d3, d4]
+		detectives = [d1, d2]
 		game = Game(detectives, mrx, board)
 		game.turn = 0
 
@@ -37,8 +37,27 @@ class Playing():
 
 			game.play()
 
+	def recreate_bug(self, positions, cards, num_iters):
+		board = Board("tax.txt", "bus.txt", "udg.txt", "rvr.txt")   
+		d1 = Detective("Detective 1")
+		d2 = Detective("Detective 2")
+		d3 = Detective("Detective 3")
+		d4 = Detective("Detective 4")
+		mrx = MrX("Mr. X")
+		detectives = [d1, d2]
+		game = Game(detectives, mrx, board)
+		game.turn = 0
+
+		game.set_state(positions, cards, turn=0, round=11)
+		mcts = MCTS(num_iters, game)
+		mcts.search(num_iters)
+
 def main():
 	run = Playing()
+	cards = [[6, 5, 6, 2, 2],
+			[10, 8, 4, 0, 0],
+			[10, 8, 4, 0, 0]]
+	#run.recreate_bug([43, 77, 46], cards, 10000)
 	run.play_vs_mcts()
 
 if __name__ == '__main__':
