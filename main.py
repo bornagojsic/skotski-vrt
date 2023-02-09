1d46cc35aeeeccbcfe54f51e604cb395bcaf842a
Ivan Bevanda <86872168+BevandaIvan@users.noreply.github.com>
2023-02-09 14:36:24 +0100

    Added recreate_state + hidden Mr. X

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ class Playing():
 	def __init__(self):
 		pass
 
-	def play_vs_mcts(self):
+	def play_vs_mcts(self, hide_mrx=False, hide_evaluations=False):
 		num_iters = int(input("Enter number of MCTS iterations per move: "))
 		board = Board("tax.txt", "bus.txt", "udg.txt", "rvr.txt")   
 		d1 = Detective("Detective 1")
@@ -17,19 +17,21 @@ class Playing():
 		d3 = Detective("Detective 3")
 		d4 = Detective("Detective 4")
 		mrx = MrX("Mr. X")
-		detectives = [d1, d2]
+		detectives = [d1, d2, d3, d4]
 		game = Game(detectives, mrx, board)
 		game.turn = 0
 
 		while not game.is_over():
 			print("===================================")
-			game.print_positions()
-			game.print_moves()
+			game.print_positions(hide_mrx=[hide_mrx, False][game.round % 5 == 3])
+			game.print_moves(hide_mrx=[hide_mrx, False][game.round % 5 == 3])
 			print("===================================")
 
 			if game.turn == 0:
 				mcts = MCTS(num_iters, game)
 				best_move = mcts.search(num_iters)
+				if not hide_evaluations:
+					mcts.print_evaluations(mcts.tree[0], best_move[0], best_move[1])
 				game.make_move(game.players[0], best_move[0], best_move[1])
 
 				game.turn = (game.turn + 1) % (len(game.players))
@@ -37,28 +39,38 @@ class Playing():
 
 			game.play()
 
-	def recreate_bug(self, positions, cards, num_iters):
+	def recreate_state(self, positions, cards, num_iters):
 		board = Board("tax.txt", "bus.txt", "udg.txt", "rvr.txt")   
 		d1 = Detective("Detective 1")
 		d2 = Detective("Detective 2")
 		d3 = Detective("Detective 3")
 		d4 = Detective("Detective 4")
 		mrx = MrX("Mr. X")
-		detectives = [d1, d2]
+		detectives = [d1, d2, d3, d4]
 		game = Game(detectives, mrx, board)
 		game.turn = 0
 
 		game.set_state(positions, cards, turn=0, round=11)
+		print("===================================")
+		game.print_positions()
+		game.print_moves()
+		print("===================================")
 		mcts = MCTS(num_iters, game)
 		mcts.search(num_iters)
 
 def main():
 	run = Playing()
-	cards = [[6, 5, 6, 2, 2],
+
+	"""positions = [128, 186, 116, 55, 161]
+	cards = [[4, 3, 2, 5, 2],
 			[10, 8, 4, 0, 0],
-			[10, 8, 4, 0, 0]]
-	#run.recreate_bug([43, 77, 46], cards, 10000)
-	run.play_vs_mcts()
+			[10, 8, 4, 0, 0],
+			[10, 8, 4, 0, 0],
+			[10, 8, 4, 0, 0],]
+
+	run.recreate_state(positions, cards, 50000)"""
+
+	run.play_vs_mcts(hide_mrx=True, hide_evaluations=False)
 
 if __name__ == '__main__':
 	main()
